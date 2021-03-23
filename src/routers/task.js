6adfde295573419fc6d35a8296b1254121accf5d
briandizon <bvdizon@gmail.com>
2021-03-23 15:51:10 +0800

    fetching tasks for specific user

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 
 const Task = require('../models/task');
+const User = require('../models/user');
 
 // endpoint for creating tasks
-router.post('/tasks', async (req, res) => {
-  const task = new Task(req.body);
+router.post('/tasks/', auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id,
+  });
   try {
     await task.save();
     res.status(201).send(task);
@@ -14,20 +19,31 @@ router.post('/tasks', async (req, res) => {
   }
 });
 
-// endpoint for fetching tasks
-router.get('/tasks', async (req, res) => {
+// endpoint for fetching tasks for specific user
+router.get('/tasks', auth, async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    res.status(202).send(tasks);
+    /**
+     * Alternative: 
+        const tasks = await Task.find({ owner: req.user._id });
+        res.status(202).send(tasks);
+     */
+
+    await req.user.populate('tasks').execPopulate();
+    res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
 // endpoint for fetching a single task by ID
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findById(req.params.id);
+    // using Model.findOne() and passing task id and owner
+    // owner must come from the _id of the authenticated user
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
     if (!task)
       return res.status(404).send("You don't have a task with that ID.");
     res.status(200).send(task);
